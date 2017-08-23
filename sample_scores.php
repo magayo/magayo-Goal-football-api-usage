4bb2068bbc51903c1542f7ace01aa6c035566d66
magayo <magayo10@gmail.com>
2017-08-23 20:22:57 +0800

    Add files via upload

---
+++ sample_scores.php
@@ -0,0 +1,29 @@
+<?
+	// Replace with your API key
+	$api_key = "hXJDjsp8I6RY";
+
+	$league = "E";
+	$match_id = 9303;
+	
+	$url = "https://www.magayogoal.com/api/scores.php?api_key=$api_key&league=$league&match_id=$match_id";
+	
+	$ch = curl_init();  
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 15); 
+	curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+	$json_string = curl_exec($ch);
+	$parsed_json = json_decode($json_string);
+
+	$error = $parsed_json->error;
+	$status = $parsed_json->status;
+	$played = $parsed_json->played;
+	$home = $parsed_json->home;
+	$away = $parsed_json->away;
+	$home_score = $parsed_json->home_score;
+	$away_score = $parsed_json->away_score;
+	
+	echo "$error<br>";
+	echo "$match_id, $status, $played, $home, $away, $home_score, $away_score<br>";
+
+?>
